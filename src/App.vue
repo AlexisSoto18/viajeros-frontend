3faf4414b4443cb00bc345127b4c79bcca27ee0d
Alexis Soto Dimas <alexissotodimas09@gmail.com>
2025-07-23 09:59:09 -0600

    small changues in the nav

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,13 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.png" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="@/assets/logo.png" width="250" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="Bienvenido!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/Home">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
